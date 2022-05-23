73797652bcb55099efce3673eaad7fe8a6d5e663
YYouFuYuan <1098091827@qq.com>
2022-05-23 16:56:44 +0800

    v2 完成用户上线

--- server.go
+++ server.go
@@ -3,25 +3,67 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	//在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex //锁
+
+	//消息广播的channel
+	Message chan string
 }
 
 //创建一个server接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
+//监听message广播消息的goroutine
+func (this *Server) LinstenMessage() {
+	for {
+		//从channel取出消息
+		msg := <-this.Message
+		//所有用户广播，发送该消息
+		this.mapLock.Lock()
+		for _, client := range this.OnlineMap {
+			client.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+//服务器广播消息
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn) {
 	//处理链接
-	fmt.Println("处理流程")
+
+	//用户上线，将用户加入map
+	user := NewUser(conn) //创建用户
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	this.BroadCast(user, "已上线")
+
+	//先阻塞
+	select {}
 }
 
 //启动服务器的接口
@@ -35,6 +77,8 @@ func (this *Server) Start() {
 	// close listen socket
 	defer listener.Close()
 
+	//启动监听message
+	go this.LinstenMessage()
 	for {
 		// accept
 		conn, err := listener.Accept()
