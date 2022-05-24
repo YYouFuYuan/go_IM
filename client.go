b1619401f7a4a2bc752ba891970a6d68ace9b118
YYouFuYuan <1098091827@qq.com>
2022-05-24 10:39:45 +0800

    v9.2 客户端解析命令行参数

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -29,8 +30,19 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+//解析命令行 ./client -ip 127.0.0.1 -port 8888
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址，默认(127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口(8888)")
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	//命令行解析
+	flag.Parse()
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>>>>>>link server error.........")
 		return
